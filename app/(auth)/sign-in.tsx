f107dac2ee3919acc65b62256c88d6375fed3e62
Muanester <stevenmutisya999@gmail.com>
2026-06-20 11:09:48 +0300

    appwrite + sign-up page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,37 @@
-import { router } from "expo-router";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link } from "expo-router";
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-      <Button title="Sign up" onPress={() => router.push("/sign-up")} />
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        placeholder="Enter your email"
+        value=""
+        onChangeText={() => {}}
+        label="Email"
+        keyboardType="email-address"
+      />
+
+      <CustomInput
+        placeholder="Enter your password"
+        value=""
+        onChangeText={() => {}}
+        label="Password"
+        secureTextEntry={true}
+      />
+      <CustomButton title="Sign In" />
+
+      <View className="flex-row gap-2 justify-center">
+        <Text className="base-regular text-gray-100">
+          Don&apos;t have an account?
+        </Text>
+        <Link href="/sign-up" className="base-bold text-primary">
+          Sign Up
+        </Link>
+      </View>
     </View>
   );
 };
